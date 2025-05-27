1900a63e4ec5847456851a54d9791bd3b1efb770
kathlourenco <kathclairesl@gmail.com>
2025-05-27 14:02:29 -0300

    test: add first code

---
+++ ex01.java
@@ -0,0 +1,17 @@
+public class ex01 {
+    public static void main(String[] args) {
+        int idade;
+        double salario, altura;
+        char genero;
+        String nome;
+        idade = 18;
+        salario = 9000.8;
+        altura = 1.58;
+        genero = 'f';
+        nome = "kath";
+
+        System.out.print("A idade é igual a " + idade + " anos \n");
+        System.out.println("O nome inserido é " + nome + "!");
+        System.out.println("O salário é: " + salario );
+    }
+}
