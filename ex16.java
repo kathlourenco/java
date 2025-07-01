5b629652e5788cf001237b21beeeacd1abd43916
Kathleen Lourenço <kathclairesl@gmail.com>
2025-07-01 17:39:24 -0300

    test: add estrutura repetitiva

---
+++ ex16.java
@@ -0,0 +1,14 @@
+public class ex16{
+    public static void main(String [] args){
+        int num;
+
+        num = 1;
+
+        while(num < 100){
+            if(num % 2 == 0){
+                System.out.println(num);
+            }
+            num = num + 1;
+        }
+    }
+}
