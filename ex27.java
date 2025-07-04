de229e14b53c7c6ce09ab1cd8f0e4034669eed63
Kathleen Lourenço <kathclairesl@gmail.com>
2025-07-03 22:38:47 -0300

    test: add funcao string

---
+++ ex27.java
@@ -0,0 +1,12 @@
+import javax.print.DocFlavor;
+
+public class ex27 {
+    public static void main(String[] args) {
+        String tal = "abc DEF hij KLM";
+
+        String s01 = tal.toLowerCase();
+
+        System.out.println(tal);
+        System.out.println(s01);
+    }
+}
